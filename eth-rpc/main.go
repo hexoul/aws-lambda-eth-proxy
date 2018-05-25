414085bf32751b7c321abc829cffc2d51d96ca0e
hexoul <crosien@gmail.com>
2018-05-25 12:35:25 +0900

    Apply eth-rpc/json

--- eth-rpc/main.go
+++ eth-rpc/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -13,25 +12,6 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
-
-type RpcRequest struct {
-	Jsonrpc string        `json:"jsonrpc"`
-	Id      int32         `json:"id"`
-	Method  string        `json:"method"`
-	Params  []interface{} `json:"params"`
-}
-
-type RpcError struct {
-	Code    int    `json:code"`
-	Message string `json:message"`
-}
-
-type RpcResponse struct {
-	Jsonrpc string                 `json:"jsonrpc"`
-	Id      int32                  `json:"id"`
-	Result  map[string]interface{} `json:"result"`
-	Error   RpcError               `json:"error"`
-}
 
 func DoRpc(targetUrl string, msg string) string {
 	reqBody := bytes.NewBufferString(msg)
@@ -52,16 +32,14 @@ func DoRpc(targetUrl string, msg string) string {
 
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// Validate RPC request
-	var req RpcRequest
-	json.Unmarshal([]byte(request.Body), &req)
+	req := GetRpcRequestFromJson(request.Body)
 	fmt.Printf("%#v\n", req)
 
 	// Forward RPC request to Ether node
 	respBody := DoRpc(TestnetUrl, request.Body)
 
 	// Relay a response from the node
-	var resp RpcResponse
-	json.Unmarshal([]byte(respBody), &resp)
+	resp := GetRpcResponse(respBody)
 	fmt.Printf("%#v\n", resp)
 	retCode := 200
 	if resp.Error.Code != 0 {
