1d0f63f6bd3e604165123e676d29d1c51de9d9c4
hexoul <crosien@gmail.com>
2018-06-20 16:59:21 +0900

    Fix annotation of common package

--- common/config.go
+++ common/config.go
@@ -1,12 +1,18 @@
 package common
 
+// DbConfigResult represents a row from DB query result
 type DbConfigResult struct {
-	Property string `json: "Property"`
-	Value    string `json: "Value"`
+	// Property indicates DB column named "Property"
+	Property string `json:"property"`
+	// Value
+	Value string `json:"value"`
 }
 
 const (
-	DbConfigTblName  = "Config"
+	// DbConfigTblName is a config table name
+	DbConfigTblName = "Config"
+	// DbConfigPropName is a property colum name
 	DbConfigPropName = "Property"
-	DbConfigValName  = "Value"
+	// DbConfigValName is a value colum name
+	DbConfigValName = "Value"
 )
