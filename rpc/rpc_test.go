8d7f6d72702b3ffe6829b81d80c66686ad0720aa
hexoul <crosien@gmail.com>
2018-06-22 16:53:58 +0900

    rpc: make benchmark to compare EthClient and HttpClient

--- rpc/rpc_test.go
+++ rpc/rpc_test.go
@@ -1,10 +1,62 @@
 package rpc
 
 import (
+	"context"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/hexoul/aws-lambda-eth-proxy/json"
 )
+
+func TestEthClient(t *testing.T) {
+	//client, err := ethclient.Dial("https://rinkeby.infura.io")
+	client, err := ethclient.Dial(TestnetUrls[0])
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	_, err = client.SuggestGasPrice(context.Background())
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+}
+
+func BenchmarkEthClient(b *testing.B) {
+	client, err := ethclient.Dial(TestnetUrls[0])
+	if err != nil {
+		b.Fatalf("%s", err)
+	}
+
+	address := common.HexToAddress("0x00F912f1F41203DaE29b37fc18db8Dbd3cA9833F")
+	errCnt := 0
+	for i := 0; i < b.N; i++ {
+		_, err := client.BalanceAt(context.Background(), address, nil)
+		if err != nil {
+			errCnt++
+		}
+	}
+	b.Logf("errCnt %d", errCnt)
+}
+
+func BenchmarkHttpClient(b *testing.B) {
+	r := GetInstance(Testnet)
+	req := json.RPCRequest{
+		Jsonrpc: "2.0",
+		ID:      1,
+		Method:  "eth_getBalance",
+	}
+	req.Params = append(req.Params, "0x00F912f1F41203DaE29b37fc18db8Dbd3cA9833F")
+	req.Params = append(req.Params, "latest")
+
+	errCnt := 0
+	for i := 0; i < b.N; i++ {
+		_, err := r.DoRPC(req)
+		if err != nil {
+			errCnt++
+		}
+	}
+	b.Logf("errCnt %d", errCnt)
+}
 
 func TestRefreshUrlList(t *testing.T) {
 	r := GetInstance(Testnet)
