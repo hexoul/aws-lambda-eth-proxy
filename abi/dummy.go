84562d4c6426b69f9987b554087c5649eb272bc9
hexoul <crosien@gmail.com>
2018-06-22 15:28:20 +0900

    abi: sup. dummy

--- abi/dummy.go
+++ abi/dummy.go
@@ -2,6 +2,7 @@ package abi
 
 import (
 	"math/big"
+	"sync/atomic"
 
 	"github.com/hexoul/aws-lambda-eth-proxy/crypto"
 	"github.com/hexoul/aws-lambda-eth-proxy/json"
@@ -39,7 +40,14 @@ func DummySendTransactionWithSign(abi abi.ABI, targetNet, to, name string, input
 	}
 
 	c := crypto.GetDummy()
-	tx := types.NewTransaction(0, common.HexToAddress(to), big.NewInt(0), uint64(gasLimit), big.NewInt(int64(gasPrice)), data)
+	r := rpc.GetInstance(targetNet)
+	mutex.Lock()
+	defer mutex.Unlock()
+	if c.Txnonce == 0 {
+		c.Txnonce = r.GetTransactionCount(c.Address)
+	}
+	nonce := atomic.LoadUint64(&c.Txnonce)
+	tx := types.NewTransaction(nonce, common.HexToAddress(to), big.NewInt(0), uint64(gasLimit), big.NewInt(int64(gasPrice)), data)
 	tx, err = c.SignTx(tx)
 	if err != nil {
 		return
@@ -50,12 +58,14 @@ func DummySendTransactionWithSign(abi abi.ABI, targetNet, to, name string, input
 		return
 	}
 
-	r := rpc.GetInstance(targetNet)
 	respStr, err := r.SendRawTransaction(rlpTx)
 	if err != nil {
 		return
 	}
 
 	resp = json.GetRPCResponseFromJSON(respStr)
+	if resp.Error == nil {
+		atomic.AddUint64(&c.Txnonce, 1)
+	}
 	return
 }
