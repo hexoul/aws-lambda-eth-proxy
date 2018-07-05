3066190e8c09845f82a03e6a4904593e736bda50
hexoul <crosien@gmail.com>
2018-07-05 13:58:27 +0900

    update toml for multiformats used in go-ipfs-api because it is not tracked automatically by dep

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/hexoul/aws-lambda-eth-proxy/crypto"
+	_ "github.com/hexoul/aws-lambda-eth-proxy/ipfs"
 	"github.com/hexoul/aws-lambda-eth-proxy/json"
 	"github.com/hexoul/aws-lambda-eth-proxy/predefined"
 	"github.com/hexoul/aws-lambda-eth-proxy/rpc"
