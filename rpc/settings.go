893811179e0f2d5eb183594cfe921d017ed20722
hexoul <crosien@gmail.com>
2018-11-16 13:47:09 +0900

    refactor and update lock

--- rpc/settings.go
+++ rpc/settings.go
@@ -1,10 +1,13 @@
 package rpc
 
-// MainnetUrls is a URL list for mainnet
-var MainnetUrls = []string{""}
-
-// TestnetUrls is a URL list for testnet
-var TestnetUrls = []string{""}
+var (
+	// MainnetUrls is a URL list for mainnet
+	MainnetUrls = []string{""}
+	// TestnetUrls is a URL list for testnet
+	TestnetUrls = []string{""}
+)
 
 // ContentType is a content-type for JSON-RPC
-const ContentType = "application/json"
+const (
+	ContentType = "application/json"
+)
